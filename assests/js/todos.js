d2fe6d3112240b0be92c678df4eab002e03eac87
Bhaskar Paul <53259501+BhaskarPaul@users.noreply.github.com>
2020-10-18 12:45:14 +0530

    Add files via upload

---
+++ assests/js/todos.js
@@ -0,0 +1,22 @@
+// click and stick
+
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+// delete that element on clicking X
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// add new todos
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        let todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
